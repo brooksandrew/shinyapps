879087ea729157855c029b6bb51c09f2eaa373be
Andrew Brooks <andrewbrooksct@gmail.com>
2014-10-12 14:32:24 -0400

    wmataAPI app working with reactivty of map to bus route. need to get text reactive

--- busMapHistory/server.R
+++ busMapHistory/server.R
@@ -1,29 +1,39 @@
 ## reactiveTimer. See ?reactiveTimer
 #setwd('/Users/ajb/Documents/shiny/nextbus')
 
+## setting up data to be reactive
 
+maxRows <- 100
 baseurl <- 'http://api.wmata.com/Bus.svc/json/jBusPositions?routeId='
+route <- '64' 
 endurl <- '&includingVariations=true&lat=0&lon=0&radius=0&api_key='
 key <- 'x42rp9qg6jjjydn2u8ng8stx'
 
+request <- paste(baseurl, route, endurl, key, sep='')
+temp <- getURL(URLencode(request), ssl.verifypeer=F)
+busdf <- data.frame(fromJSON(temp, simplifyVector=T))
+
 shinyServer(function(input, output, session) {
+  wait <- reactiveTimer(50000, session)
   
   output$mymap <- renderPlot ({
-    
-    route <- input$busid 
-    request <- paste(baseurl, route, endurl, key, sep='')
+    wait()  
     temp <- getURL(URLencode(request), ssl.verifypeer=F)
-    busdf <<- data.frame(fromJSON(temp, simplifyVector=T))
-
-    ## just keeping maxRows in busdf    
+    dl <<- data.frame(fromJSON(temp, simplifyVector=T))
+    busdf <<- rbind(busdf, dl)
+    
+    ## just keeping maxRows in busdf
+    if(nrow(busdf)>maxRows) busdf <<- busdf[(nrow(busdf)-maxRows):nrow(busdf), ]
+    
     map <- get_map(location = c(busdf$BusPositions.Lon[1], busdf$BusPositions.Lat[1]), zoom=13)
     mapPoints <- ggmap(map) + 
-      geom_point(aes(x=BusPositions.Lon, y=BusPositions.Lat), data=busdf, alpha=0.85, size=7)
+      geom_point(aes(x=BusPositions.Lon, y=BusPositions.Lat), data=busdf, alpha=0.25)
     
     plot(mapPoints)
   })
   
   output$mytext <- renderText ({
+    wait()
     busdf[nrow(busdf),1]
   })
   
--- busMapHistory/ui.R
+++ busMapHistory/ui.R
@@ -5,10 +5,10 @@ require('shiny')
 require('ggmap')
 
 key <- 'x42rp9qg6jjjydn2u8ng8stx'
-requestBusRoutes <- paste('http://api.wmata.com/Bus.svc/json/jRoutes?api_key=', key, sep='')
-tempBusRoutes <- getURL(URLencode(requestBusRoutes), ssl.verifypeer=F)
-busesRoutes <- data.frame(fromJSON(tempBusRoutes, simplifyVector=T))
-buses <- busesRoutes[,2]
+request <- paste('http://api.wmata.com/Bus.svc/json/jRoutes?api_key=', key, sep='')
+temp <- getURL(URLencode(request), ssl.verifypeer=F)
+busesdf <- data.frame(fromJSON(temp, simplifyVector=T))
+buses <- busesdf[,2]
 
 shinyUI(fluidPage(
   titlePanel("64 Bus Map"),
