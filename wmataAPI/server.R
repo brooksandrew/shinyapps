64181559cc50803eb14e345407405503fb9459b8
Andrew Brooks <andrewbrooksct@gmail.com>
2014-10-12 15:58:49 -0400

    adding busstop selectInput

--- wmataAPI/server.R
+++ wmataAPI/server.R
@@ -8,30 +8,53 @@ key <- 'x42rp9qg6jjjydn2u8ng8stx'
 
 shinyServer(function(input, output, session) {
   
+  ## GET DATA FROM API
   data <- reactive({
     route = input$busid 
-    request = paste(baseurl, route, endurl, key, sep='')
-    temp = getURL(URLencode(request), ssl.verifypeer=F)
-    busdf = data.frame(fromJSON(temp, simplifyVector=T))
+    request <- paste(baseurl, route, endurl, key, sep='')
+    temp <- getURL(URLencode(request), ssl.verifypeer=F)
+    busdf <- data.frame(fromJSON(temp, simplifyVector=T))
   })
   
+  ## MAP
   output$mymap <- renderPlot ({
     input$updateid
     busdf <- data()
-    ## just keeping maxRows in busdf   
     
     map <- get_map(location = c(busdf$BusPositions.Lon[1], busdf$BusPositions.Lat[1]), zoom=13)
     mapPoints <- ggmap(map) + 
-      geom_point(aes(x=BusPositions.Lon, y=BusPositions.Lat), data=busdf, alpha=0.85, size=7)
+      geom_point(aes(x=BusPositions.Lon, y=BusPositions.Lat), data=busdf, alpha=0.85, size=7) +
+      ylab('') + xlab('') + theme(axis.ticks=element_blank(), axis.text.x=element_blank(), axis.text.y=element_blank())
     
     plot(mapPoints)
   })
   
+  ## TEXT
   output$mytext <- renderText ({
     input$updateid
     busdf <- data()
     busdf[1,1]
   })
+  
+  ## DATATABLE
+  output$mytable <- renderDataTable ({
+    input$updateid
+    df2print <- data()
+    df2print <- df2print[,1:3]
+  })
+  
+  
+  output$stopsid <- renderUI({
+    requestBusStops <- paste('http://api.wmata.com/Bus.svc/json/jRouteDetails?routeId=', input$busid, '&date=', Sys.Date(), '&api_key=', key, sep='')
+    
+    requestBusStops <- paste('http://api.wmata.com/Bus.svc/json/jRouteDetails?routeId=', '64', '&date=', Sys.Date(), '&api_key=', key, sep='')
+    temp <- getURL(URLencode(requestBusStops), ssl.verifypeer=F)
+    stops <- fromJSON(temp, simplifyVector=T)
+    stopsdf <- stops$Direction0$Stops
+    selectInput("stops", "Choose a bus stop", stopsdf$Name)
+                       
+  })
+  
   
 })
 
--- wmataAPI/ui.R
+++ wmataAPI/ui.R
@@ -4,11 +4,18 @@ require('ggmap')
 require('shiny')
 require('ggmap')
 
+## GETTING BUS ROUTES
 key <- 'x42rp9qg6jjjydn2u8ng8stx'
 requestBusRoutes <- paste('http://api.wmata.com/Bus.svc/json/jRoutes?api_key=', key, sep='')
 tempBusRoutes <- getURL(URLencode(requestBusRoutes), ssl.verifypeer=F)
 busesRoutes <- data.frame(fromJSON(tempBusRoutes, simplifyVector=T))
 buses <- busesRoutes[,2]
+
+
+## GETTING BUS STOPS
+requestBusStops <- paste('http://api.wmata.com/Bus.svc/json/jRouteDetails?routeId=', 64, '&date=', Sys.Date(), '&api_key=', key, sep='')
+
+
 
 shinyUI(fluidPage(
   titlePanel("64 Bus Map"),
@@ -16,14 +23,16 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput('busid', 'Choose a bus', buses, selected='64'),
-      actionButton('updateid', 'Refresh data')
+      actionButton('updateid', 'Refresh data'),
+      uiOutput("stopsid")
     ),
     
     mainPanel(
-      tabsetPanel(
+      #tabsetPanel(
         tabPanel("Map", plotOutput("mymap", "600px", "600px")),
-        tabPanel("Text", textOutput("mytext"))
-      )
+        tabPanel("Text", textOutput("mytext")),
+        tabPanel("Table", dataTableOutput("mytable"))
+      #)
     )  
     
   )
